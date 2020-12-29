20bdb17bdeb33c6e2ded5ce73b1333f119630345
Shubham <gupta.shubh.0016@gmail.com>
2020-12-29 23:13:57 +0530

    removed todo task

--- index.js
+++ index.js
@@ -18,57 +18,57 @@ mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
 // require('../node-mongo-app/routes/easyRoutes')(app);
 
 // // GET METHOD
-app.get("/", (req, res) => {
-    TodoTask.find({}, (err, tasks) => {
-        res.send(tasks);
-        // res.render("todo.ejs", { todoTasks: tasks });
-    });
-});
+// app.get("/", (req, res) => {
+//     TodoTask.find({}, (err, tasks) => {
+//         res.send(tasks);
+//         // res.render("todo.ejs", { todoTasks: tasks });
+//     });
+// });
 
-app
-    .route("/add")
-    .post(async (req, res) => {
-        // .post('/',async (req, res) => {
-        console.log("req.body.content ", req)
-        const todoTask = new TodoTask({
-            content: req.body.content
-        });
-        try {
-            await todoTask.save();
-            res.send({ "result": "success" });
-            // res.redirect("/");
-        } catch (err) {
-            res.send({ "error": err });
-            // res.redirect("/");
-        }
-    });
+// app
+//     .route("/add")
+//     .post(async (req, res) => {
+//         // .post('/',async (req, res) => {
+//         console.log("req.body.content ", req)
+//         const todoTask = new TodoTask({
+//             content: req.body.content
+//         });
+//         try {
+//             await todoTask.save();
+//             res.send({ "result": "success" });
+//             // res.redirect("/");
+//         } catch (err) {
+//             res.send({ "error": err });
+//             // res.redirect("/");
+//         }
+//     });
 
 //UPDATE
-app
-    .route("/edit/:id")
-    .get((req, res) => {
-        const id = req.params.id;
-        TodoTask.find({}, (err, tasks) => {
-            res.render("todoEdit.ejs", { todoTasks: tasks, idTask: id });
-        });
-    })
-    .post((req, res) => {
-        const id = req.params.id;
-        TodoTask.findByIdAndUpdate(id, { content: req.body.content }, err => {
-            if (err) return res.send(500, err);
-            res.redirect("/");
-        });
-    });
+// app
+//     .route("/edit/:id")
+//     .get((req, res) => {
+//         const id = req.params.id;
+//         TodoTask.find({}, (err, tasks) => {
+//             res.render("todoEdit.ejs", { todoTasks: tasks, idTask: id });
+//         });
+//     })
+//     .post((req, res) => {
+//         const id = req.params.id;
+//         TodoTask.findByIdAndUpdate(id, { content: req.body.content }, err => {
+//             if (err) return res.send(500, err);
+//             res.redirect("/");
+//         });
+//     });
 
 //DELETE
-app.route("/remove/:id").get((req, res) => {
-    const id = req.params.id;
-    TodoTask.findByIdAndRemove(id, err => {
-        if (err) return res.send(500, err);
-        // res.redirect("/");
-        res.send({ "result": "success" })
-    });
-});
+// app.route("/remove/:id").get((req, res) => {
+//     const id = req.params.id;
+//     TodoTask.findByIdAndRemove(id, err => {
+//         if (err) return res.send(500, err);
+//         // res.redirect("/");
+//         res.send({ "result": "success" })
+//     });
+// });
 
 //easy business model- APIS
 
